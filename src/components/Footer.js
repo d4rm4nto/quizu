d576736cf60b988bb3c730f63764c93a7628fe9a
d4rm4anto <darmanto2098@gmail.com>
2023-03-18 23:26:35 +0700

    perubahan footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,13 +14,14 @@ const Footer = () => {
                             <img className='w-20 h-30 text-gray-300' src={LogoQuizzU} alt="logo-quiz" />
                         </div>
                         <div className='mt-5'>
-                        QuizU adalah sebuah platform pembelajaran interaktif untuk siswa SMA/SMK yang menyediakan bank soal, latihan, dan fitur pelacakan kemajuan. Dengan QuizU, siswa dapat belajar dengan cara yang lebih menyenangkan dan efektif.
+                            QuizU adalah sebuah platform pembelajaran interaktif untuk siswa SMA/SMK yang menyediakan bank soal, latihan, dan fitur pelacakan kemajuan. Dengan QuizU, siswa dapat belajar dengan cara yang lebih menyenangkan dan efektif.
                         </div>
                     </Col>
                     <Col sm md={4}>
                         <h5>Get Started</h5>
                         <h6>Mulai</h6>
                         <h6>Menuju Quiz</h6>
+                        <h6>Jangkrik</h6>
                     </Col>
                     <Col sm md={4}>
                         <h5>Kontak Kami</h5>
