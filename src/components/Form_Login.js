4e21749ccbac50a5664b1d19665478209723d196
novalarrg <novaljr197@gmail.com>
2023-03-27 14:21:18 +0700

    Modifikasi pages login & menghapus default react

--- src/components/Form_Login.js
+++ src/components/Form_Login.js
@@ -1,44 +1,105 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col } from "react-bootstrap";
+import Logo from "../assets/img/Logo.png";
+import LogoGoogle from "../assets/img/LogoGoogle.svg";
 
 export const Form_Login = () => {
-    return (
-        <div className="container-login bg-transparent d-flex justify-cente align-items-sm-center h-screen">
-            <div class="container my-5">
-                <div class="row justify-content-center">
-                    <div class="col-md-6">
-                        <div class="card border border-slate-300 bg-transparent shadow-lg">
-                            <div class="card-header text-white">
-                                <h4 class="card-title mb-0 text-center">Login</h4>
-                            </div>
-                            <div class="card-body">
-                                <form>
-                                    <div class="mb-3 text-white">
-                                        <label for="username" class="form-label">Username</label>
-                                        <input type="text" class="form-control" id="username" placeholder="Masukan Nama Pengguna" required />
-                                    </div>
-                                    <div class="mb-3 text-white">
-                                        <label for="password" class="form-label">Password</label>
-                                        <input type="password" class="form-control" id="password" placeholder="Masukan Kata Sandi" required />
-                                    </div>
-                                    <button type="submit" class="btn btn-primary w-100 mt-3">Masuk</button>
-                                </form>
-                            </div>
-                            <div className="text-white text-center">
-                                <hr /> Atau Masuk Menggunakan <hr />
-                            </div>
-                            <div className='text-center mt-3' style={{ color: "#4285F4" }}>
-                                <FontAwesomeIcon icon={faGoogle} />
-                            </div>
-                            <div className='text-center mt-3 mb-3 text-white'>
-                                <p>Belum Mempunyai Akun ?<Link className='text-danger text-decoration-none' to="/pendaftaran"><b>Daftar Sekarang</b></Link></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+  return (
+    <Row>
+      <Col sm md={5}>
+        <div className="flex top-10">
+          <img className="p-5" width={240} src={Logo}></img>
         </div>
-    )
-}
+      </Col>
+      <Col sm md={7}>
+        <div className="container-login bg-white d-flex justify-cente align-items-sm-center h-screen rounded-start-4">
+          <div className="container my-5">
+            <div className="row justify-content-center">
+              <div className="col-md-7  ">
+                <div className="card-header text-balck">
+                  <h4 className="card-title fw-bold mb-5 text-center">
+                    Login QuizU
+                  </h4>
+                </div>
+                <div className="card-body">
+                  <form>
+                    <div className="mb-3 text-black fw-semibold">
+                      <label htmlFor="username" className="form-label">
+                        Username
+                      </label>
+                      <input
+                        type="text"
+                        className="form-control"
+                        id="username"
+                        placeholder="Masukan Username"
+                        required
+                      />
+                    </div>
+                    <div className="mb-3 text-black fw-semibold">
+                      <label htmlFor="password" className="form-label">
+                        Password
+                      </label>
+                      <input
+                        type="password"
+                        className="form-control"
+                        id="password"
+                        placeholder="Masukan Password"
+                        required
+                      />
+                    </div>
+                    <button
+                      type="submit"
+                      className="btn btn-primary w-100 mt-4 fw-bold"
+                    >
+                      Masuk
+                    </button>
+                    <div className="flex justify-center mt-2">
+                      <div className="col-5">
+                        <hr />
+                      </div>
+                      <div>
+                        <p className="text-secondary mx-2">atau</p>
+                      </div>
+                      <div className="col-5">
+                        <hr />
+                      </div>
+                    </div>
+                    <button
+                      type="submit"
+                      className="btn btn-outline-secondary w-100 fw-bold"
+                      style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        style={{ padding: "4px" }}
+                        src={LogoGoogle}
+                        width={30}
+                      ></img>{" "}
+                      Masuk Dengan Google
+                    </button>
+                  </form>
+                </div>
+
+                <div className="text-center mt-3 mb-3 text-balck">
+                  <p>
+                    Belum punya akun ?
+                    <Link
+                      className="text-danger text-decoration-none"
+                      to="/pendaftaran"
+                    >
+                      <b className="italic"> Daftar</b>
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Col>
+    </Row>
+  );
+};
--- src/pages/Pendaftaran.jsx
+++ src/pages/Pendaftaran.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { faGoogle } from '@fortawesome/free-brands-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { Link } from "react-router-dom";
+// import { faGoogle } from '@fortawesome/free-brands-svg-icons'
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export const Pendaftaran = () => {
   return (
-    <div className="container-pendaftaran bg-primary d-flex justify-cente align-items-sm-center h-screen">
+    <div class="container-pendaftaran bg-primary d-flex justify-cente align-items-sm-center h-screen">
       <div class="container my-5">
         <div class="row justify-content-center">
           <div class="col-md-6">
@@ -16,36 +16,70 @@ export const Pendaftaran = () => {
               <div class="card-body">
                 <form>
                   <div class="mb-3 text-white">
-                    <label for="name" class="form-label">Nama Lengkap</label>
-                    <input type="text" class="form-control" id="username" placeholder="Masukan Nama Lenhkap" required />
+                    <label for="name" class="form-label">
+                      Nama Lengkap
+                    </label>
+                    <input
+                      type="text"
+                      class="form-control"
+                      id="username"
+                      placeholder="Masukan Nama Lenhkap"
+                      required
+                    />
                   </div>
 
                   <div class="mb-3 text-white">
-                    <label for="email" class="form-label">Masukan Email Anda</label>
-                    <input type="email" class="form-control" id="username" placeholder="Masukan Alamat Email" required />
+                    <label for="email" class="form-label">
+                      Masukan Email Anda
+                    </label>
+                    <input
+                      type="email"
+                      class="form-control"
+                      id="username"
+                      placeholder="Masukan Alamat Email"
+                      required
+                    />
                   </div>
 
                   <div class="mb-3 text-white">
-                    <label for="password" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="password" placeholder="Masukan Kata Sandi" required />
+                    <label for="password" class="form-label">
+                      Password
+                    </label>
+                    <input
+                      type="password"
+                      class="form-control"
+                      id="password"
+                      placeholder="Masukan Kata Sandi"
+                      required
+                    />
                   </div>
 
-                  <button type="submit" class="btn btn-primary w-100 mt-3">Daftar</button>
+                  <button type="submit" class="btn btn-primary w-100 mt-3">
+                    Daftar
+                  </button>
                 </form>
               </div>
               <div className="text-white text-center">
                 <hr /> Atau Masuk Menggunakan <hr />
               </div>
-              <div className='text-center mt-3' style={{ color: "#4285F4" }}>
-                <FontAwesomeIcon icon={faGoogle} />
+              <div className="text-center mt-3" style={{ color: "#4285F4" }}>
+                {/* <FontAwesomeIcon icon={faGoogle} /> */} Google
               </div>
-              <div className='text-center mt-3 mb-3 text-white'>
-                <p>Sudah Mempunyai Akun ?<Link className='text-danger text-decoration-none' to="/login"><b>Masuk Sekarang</b></Link></p>
+              <div className="text-center mt-3 mb-3 text-white">
+                <p>
+                  Sudah Mempunyai Akun ?
+                  <Link
+                    className="text-danger text-decoration-none"
+                    to="/login"
+                  >
+                    <b>Masuk Sekarang</b>
+                  </Link>
+                </p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
